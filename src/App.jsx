0362a6c22f3d6c97603ade6d63a5bd45a5a0cae0
Khushi Patel <patelkhushi7041@gmail.com>
2025-04-20 13:26:02 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,22 @@ import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
 import Product from './Pages/Product/Product'
 import Login from './Components/LoginAndRegister/LoginPopup'
 import Register from './Components/LoginAndRegister/Register'
+import "./App.css"
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
   const [showRegister, setShowRegister] = useState(false);
   const [user, setUser] = useState(null);
+
+  // loading
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
 
   useEffect(() => {
     const savedUser = localStorage.getItem("username");
@@ -33,33 +44,32 @@ const App = () => {
 
   return (
     <>
-      <div className='app'>
-        <Navbar 
-          setShowLogin={setShowLogin} 
-          user={user}
-          setUser={handleLogout}
-        />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/cart' element={<Cart setShowLogin={setShowLogin} user={user} />} />
-          <Route path='/placeorder' element={<PlaceOrder />} />
-          <Route path='/product' element={<Product />} />
-        </Routes>
-        <Footer />
-        {showLogin && (
-          <Login
-            setShowLogin={setShowLogin}
-            setShowRegister={setShowRegister}
-            setUser={handleSetUser} 
-          />
-        )}
+      <div className="app">
+        {loading ? (
+          <div className="loader-container">
+            <div className="loader">
+              <div className="loader-ring"></div>
+              <p className="loading-text">Loading your beauty essentials...</p>
+            </div>
+          </div>
+        ) : (
+          <>
+            <Navbar setShowLogin={setShowLogin} user={user} setUser={handleLogout} />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart setShowLogin={setShowLogin} user={user} />} />
+              <Route path="/placeorder" element={<PlaceOrder />} />
+              <Route path="/product" element={<Product />} />
+            </Routes>
+            <Footer />
+            {showLogin && (
+              <Login setShowLogin={setShowLogin} setShowRegister={setShowRegister} setUser={handleSetUser} />
+            )}
 
-        {showRegister && (
-          <Register
-            setShowRegister={setShowRegister}
-            setShowLogin={setShowLogin}
-            setUser={handleSetUser} 
-          />
+            {showRegister && (
+              <Register setShowRegister={setShowRegister} setShowLogin={setShowLogin} setUser={handleSetUser} />
+            )}
+          </>
         )}
       </div>
     </>
